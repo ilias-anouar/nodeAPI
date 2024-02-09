cc76b803c7c9a07bf1161b894a24a983d422a402
ilias anouar <115695877+ilias-anouar@users.noreply.github.com>
2024-02-09 16:24:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(cors());
 app.set('trust proxy', true)
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // disabled for security on local
