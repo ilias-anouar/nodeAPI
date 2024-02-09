ecb86bf90516db061c014ea8da8323bd6f6d7b40
ilias anouar <115695877+ilias-anouar@users.noreply.github.com>
2024-02-09 16:19:16 +0000

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const controllers = require('./controllers')
 const router = express.Router();
 
-router.get('/message/save/', controllers.saveMessage)
+router.post('/message/save/', controllers.saveMessage)
 router.get('/message/read/', controllers.readMessage);
 router.get('/message/read/:name', controllers.readMessageName);
 router.get('/message/delete/:name', controllers.deleteMessage);
